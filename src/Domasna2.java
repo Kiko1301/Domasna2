addd789fdd103c768cc0c1587326a1c342818d22
Kristijan Jordanovski <jordanovskikr@gmail.com>
2023-04-03 10:34:59 +0200

    third commit

---
+++ src/Domasna2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner; 
+
+public class Domasna2 {
+public static void main(String[] args) {
+		Scanner scanner = new
+			Scanner(System.in);
+		System.out.println("Vnesete cel broj");
+		int n = scanner.nextInt();
+			if (n<0) {
+				System.out.println("Vneseniot broj e negativen");
+				return;
+			}
+		long factorial = 1;
+		for (int i=1; i<=n; i++){
+			factorial*= i;
+			if (factorial<0) {
+				System.out.println("Se sluci prelevanje");
+				return;
+			}
+			
+		}
+		System.out.println(n+"!=" + factorial);
+	}
+
+}
